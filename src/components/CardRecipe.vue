7857362cdcd962f1f12a527b5c1c9c14819f6882
mileneD <milene@intellicloud.com>
2024-10-21 09:19:03 -0400

    fix(): style

--- src/components/CardRecipe.vue
+++ src/components/CardRecipe.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="">
-
-    <v-card elevated class="mx-4 my-4 card-recipe ">
+    <v-card elevated class="mx-4 my-4 card-recipe">
       <v-card-title class="flex">{{ recipe.title }}</v-card-title>
       <v-card-text>
-        <img class="img-card" :src="recipe.image" @error="imageError = true" />
+        <img
+          class="img-card"
+          :src="recipe.image"
+          @error="imageError = true"
+          alt="Recipe Image"
+        />
         <div v-if="imageError">
-          <img class="img-card" src="https://st2.depositphotos.com/2586633/46477/v/450/depositphotos_464771766-stock-illustration-no-photo-or-blank-image.jpg" />
+          <img
+            class="img-card"
+            src="https://st2.depositphotos.com/2586633/46477/v/450/depositphotos_464771766-stock-illustration-no-photo-or-blank-image.jpg"
+            alt="Default Image"
+          />
         </div>
       </v-card-text>
     </v-card>
@@ -22,25 +30,26 @@ export default {
       required: true,
     },
   },
-  setup(){
-    const imageError = ref(false)
+  setup() {
+    const imageError = ref(false);
 
-    return{
-      imageError
-    }
-  }
-}
+    return {
+      imageError,
+    };
+  },
+};
 </script>
 
 <style scoped>
 .card-recipe {
-  width: 300px;
+  width: 300px; /* Largeur fixe de la carte */
   height: auto;
   cursor: pointer;
 }
 
 .img-card {
   width: 100%;
-  height: auto;
+  height: 150px;
+  object-fit: cover;
 }
 </style>
